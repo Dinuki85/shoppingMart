6d519dfced081d82f5629b6084c4d3638422e7de
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-20 17:21:42 +0530

    Feat:Added method for generating the autopassword

--- src/main/java/com/online/model/User.java
+++ src/main/java/com/online/model/User.java
@@ -29,7 +29,7 @@ public class User {
 
     private String password;
 
-    private USER_ROLE role;
+    private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
 
     //Whenever fetch the user list no need to fetch orders
     @JsonIgnore
