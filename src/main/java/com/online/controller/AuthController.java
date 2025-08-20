458691678566542ba28a90a8da46f0f0d31b51b1
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-20 17:50:01 +0530

    Feat:Added method for generating jwt token

---
+++ src/main/java/com/online/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.online.controller;
+
+import com.online.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+
+
+}
