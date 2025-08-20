8da212d5524ab1686122ea762a3a12006edcd786
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-20 20:01:37 +0530

    Fix:Fix the unautherized issue

---
+++ src/main/java/com/online/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.online.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    public ResponseEntity<String> homeController(){
+        return null;
+    }
+
+
+}
