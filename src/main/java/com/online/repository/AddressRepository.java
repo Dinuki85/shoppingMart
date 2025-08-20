07e20451c3ceba65f28ef46c7ee755357ceb75c9
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-20 22:43:41 +0530

    Feat:Implement a method for creating a new resturant and save it in a database

---
+++ src/main/java/com/online/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.online.repository;
+
+import com.online.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address,Long> {
+
+}
---
+++ src/main/java/com/online/service/ShopServiceImpl.java
@@ -0,0 +1,89 @@
+package com.online.service;
+
+import com.online.dto.ShopsDto;
+import com.online.model.Address;
+import com.online.model.Shop;
+import com.online.model.User;
+import com.online.repository.AddressRepository;
+import com.online.repository.ShopRepository;
+import com.online.request.CreateShopRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ShopServiceImpl  implements ShopService{
+
+    @Autowired
+    private ShopRepository shopRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+
+    @Autowired
+    private UserService userService;
+
+
+    //Method for creating a new shop and save it
+    @Override
+    public Shop createShop(CreateShopRequest req, User user) {
+        Address address = addressRepository.save(req.getAddress());
+
+        Shop shop = new Shop();
+        shop.setAddress(address);
+        shop.setContactInformation(req.getContactInformation());
+        shop.setCuisineType(req.getCuisineType());
+        shop.setDescription(req.getDescription());
+        shop.setImages(req.getImages());
+        shop.setName(req.getName());
+        shop.setOpeningHours(req.getOpeningHours());
+        shop.setRegistrationDate(LocalDateTime.now());
+        shop.setOwner(user);
+
+
+        return shopRepository.save(shop);
+    }
+
+    @Override
+    public Shop updateShop(Long ShopId, CreateShopRequest updatedShop) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteShop(Long shopId) throws Exception {
+
+    }
+
+    @Override
+    public List<Shop> getAllShops() {
+        return List.of();
+    }
+
+    @Override
+    public List<Shop> searchShop() {
+        return List.of();
+    }
+
+    @Override
+    public Shop findShopBYId(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Shop getShopByUserId(Long UserId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ShopsDto addToFavourite(Long shopId, User user) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Shop updateShopStatus(Long id) throws Exception {
+        return null;
+    }
+}
