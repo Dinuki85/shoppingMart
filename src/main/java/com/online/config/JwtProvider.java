f256cc1ed2c1c703b5a2a2ef416c4c8806eb5c87
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-20 18:14:16 +0530

    Feat:Added method for get a email from the jwt token

--- src/main/java/com/online/config/JwtProvider.java
+++ src/main/java/com/online/config/JwtProvider.java
@@ -1,5 +1,6 @@
 package com.online.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
@@ -38,6 +39,19 @@ public class JwtProvider {
 
         return jwt;
     }
+
+    //Getting the email from jwt token
+    public String getEmailFromJwtToken(String jwt){
+        jwt = jwt.substring(7);
+
+        Claims claims =   Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
+        String email = String.valueOf(claims.get("email"))  ;
+
+
+        return email;
+    }
+
 
 
 
