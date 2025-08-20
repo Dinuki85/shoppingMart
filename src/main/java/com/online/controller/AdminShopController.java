641200f2bf48b2854972a8e4e2ac6f3205666257
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-20 23:17:35 +0530

    Feat:Added api endpoint for the updateHSop

--- src/main/java/com/online/controller/AdminShopController.java
+++ src/main/java/com/online/controller/AdminShopController.java
@@ -8,10 +8,7 @@ import com.online.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //All the endpoints that related to the owner of the shop
 @RestController
@@ -23,11 +20,25 @@ public class AdminShopController {
     @Autowired
     private UserService userService;
 
-    public ResponseEntity<Shop> createSHop(@RequestBody CreateShopRequest req, @RequestHeader("Authorization" ) String jwt) throws Exception {
+    @PostMapping()
+    public ResponseEntity<Shop> createShop(@RequestBody CreateShopRequest req, @RequestHeader("Authorization" ) String jwt) throws Exception {
         User user =userService.findUserByJwtToken(jwt);
 
         Shop shop = shopService.createShop(req,user);
 
         return new ResponseEntity<>(shop, HttpStatus.CREATED);
     }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Shop> updateShop(@RequestBody CreateShopRequest req, @RequestHeader("Authorization" ) String jwt,@PathVariable Long id) throws Exception {
+        User user =userService.findUserByJwtToken(jwt);
+
+        Shop shop = shopService.updateShop(id,req);
+
+        return new ResponseEntity<>(shop, HttpStatus.CREATED);
+    }
+
+
+
+
 }
