6edbb4cddbe6164be887678761fede02c7b6847e
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-20 16:38:23 +0530

    Added for JWT VAlidating and for adding secret key

---
+++ src/main/java/com/online/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.online.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="vfhirkgjbvkisgfuabdhtwbopyhmscGFDGROGJWFUEFBUUENMLFSMLPFHDNKTOUYiurorm";
+
+    public static final String JWT_HEADER="Authorization";
+}
