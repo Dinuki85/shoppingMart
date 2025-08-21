579ad3e8305381d34d5ecdc6d4b2709e895e1aa3
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-21 17:59:51 +0530

    Feat:Added endpoints to the create Ctaegory

---
+++ src/main/java/com/online/controller/CategoryController.java
@@ -0,0 +1,35 @@
+package com.online.controller;
+
+import com.online.model.Category;
+import com.online.model.User;
+import com.online.service.CategoryService;
+import com.online.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category,
+                                                   @RequestHeader("Authorization") String jwt ) throws Exception {
+
+        User user =userService.findUserByJwtToken(jwt);
+
+        Category createdCategory=categoryService.createCategory(category.getName(),user.getId());
+
+        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+
+
+    }
+}
