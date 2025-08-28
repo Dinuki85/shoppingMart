cae4ae549efc3655b9e8cdb81917353438d122e7
Dinuki85 <prarthanadinuki2@gmail.com>
2025-08-28 23:15:02 +0530

    Feat:Added method to the update the cart item quantity

--- src/main/java/com/online/controller/CartController.java
+++ src/main/java/com/online/controller/CartController.java
@@ -2,6 +2,7 @@ package com.online.controller;
 
 import com.online.model.CartItem;
 import com.online.request.AddCartItemRequest;
+import com.online.request.UpdateCartItemRequest;
 import com.online.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,5 +20,12 @@ public class CartController {
                                                   @RequestHeader("Authorization") String jwt) throws Exception{
     CartItem cartItem = cartService.addItemToCart(req,jwt);
     return  new ResponseEntity<>(cartItem, HttpStatus.CREATED);
+    }
+    @PutMapping("/cartItem/update")
+    public ResponseEntity<CartItem> updateCartItemQuantity(
+            @RequestBody UpdateCartItemRequest req,
+           @RequestHeader("Authorization") String jwt) throws Exception{
+        CartItem cartItem = cartService.updateCartItemQuantity(req.getCartItemId(),req.getQuantity());
+        return  new ResponseEntity<>(cartItem, HttpStatus.OK);
     }
 }
