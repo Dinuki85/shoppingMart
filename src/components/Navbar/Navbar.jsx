b4edcb4b9438e04be081f31ec3db66dde854277e
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-12 22:16:58 +0530

    Feat:Added Navigation bar padding sizes for the large devices and small Devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='px-7 z-60 py-[.8rem] bg-[#e91e61] lg:px-20 justify-between'>
+
+        
+
+    </div>
   )
 }
 
