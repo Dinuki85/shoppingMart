1ce6ba03180f8931efdfd237b58b377c49ac1687
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-12 22:12:22 +0530

    Feat:Added dark background for the page using material UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { ThemeProvider } from '@emotion/react';
+import { darkTheme } from './Theme/DarkTheme';
+import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
      <Navbar/>
-    </div>
+    </ThemeProvider>
   );
 }
 
