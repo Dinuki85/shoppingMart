a40fea225f299f15c1864483f58ae039d759bafd
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-12 22:29:06 +0530

    Feat:Start adding search icon for the search grocery

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,34 @@
+import { IconButton } from '@mui/material'
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div className='px-5 z-60 py-[.8rem] bg-blue-300 lg:px-20 justify-between'>
-        <div className='flex items-center space-x-4'>
-            <div className='flex items-center space-x-5 cursor-pointer lg:mr-10'>
-                <li className='font-serif text-2xl font-extrabold logo text-green-950'>
-                    Shopping Mart
-                </li>
+    return (
+        <div className='px-5 z-60 py-[.8rem] bg-blue-300 lg:px-20 justify-between'>
+            
+                <div className='flex items-center space-x-5 cursor-pointer lg:mr-10'>
+                    <li className='font-serif text-2xl font-extrabold logo text-green-950'>
+                        Shopping Mart
+                    </li>
+                
+
+
             </div>
-            
-            
+
+            <div className='flex items-center space-x-2 lg:space-x-10'>
+                <div className=''>
+                    <IconButton>
+                        
+                    </IconButton>
+                    
+                </div>
+
+            </div>
+
+
+
         </div>
 
-
-
-
-    </div>
-  )
+    )
 }
 
 export default Navbar
