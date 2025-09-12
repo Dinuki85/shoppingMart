6c584519c7d902147edcc16821f9cba9fa4df69a
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-12 22:22:24 +0530

    Feat:Added logo of the website

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='px-7 z-60 py-[.8rem] bg-[#e91e61] lg:px-20 justify-between'>
+    <div className='px-5 z-60 py-[.8rem] bg-blue-300 lg:px-20 justify-between'>
+        <div className='flex items-center space-x-4'>
+            <div className='flex items-center space-x-5 cursor-pointer lg:mr-10'>
+                <li className='font-serif text-2xl font-extrabold logo text-green-950'>
+                    Shopping Mart
+                </li>
+            </div>
+            
+            
+        </div>
 
-        
+
+
 
     </div>
   )
