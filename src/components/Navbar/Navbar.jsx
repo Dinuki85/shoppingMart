51ba8a1888f7c56a728fde7ff377a0fea8865b84
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-19 22:07:48 +0530

    Feat:Added search icon for the website using material ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import { IconButton } from '@mui/material'
 import React from 'react'
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 const Navbar = () => {
     return (
-        <div className='px-5 z-60 py-[.8rem] bg-blue-300 lg:px-20 justify-between'>
+        <div className='flex items-center px-5 z-60 py-[.8rem] bg-blue-300 lg:px-20 justify-between'>
             
                 <div className='flex items-center space-x-5 cursor-pointer lg:mr-10'>
                     <li className='font-serif text-2xl font-extrabold logo text-green-950'>
@@ -17,9 +18,12 @@ const Navbar = () => {
 
             <div className='flex items-center space-x-2 lg:space-x-10'>
                 <div className=''>
-                    <IconButton>
-                        <SearchIcon/>
+                    <IconButton >
+                        <SearchIcon sx={{fontSize:"1.5rem"}} />
                     </IconButton>
+                    
+                </div>
+                <div>
                     
                 </div>
 
