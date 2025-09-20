2ba53346d29596768b949aae0a82f4cd294d7918
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-20 10:27:27 +0530

    Remove:Remove the bulet point of the list using navbar.css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import { red } from '@mui/material/colors';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-
+import "./Navbar.css"
 const Navbar = () => {
     return (
         <div className='flex items-center px-5 z-60 py-[.8rem] bg-blue-300 lg:px-20 justify-between'>
