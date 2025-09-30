9f799aa54c8ac4e4cacedd3e4f337a9707edde08
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-30 22:52:27 +0530

    Feat:Added banner details of the website

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar/Navbar';
 import { ThemeProvider } from '@emotion/react';
 import { darkTheme } from './Theme/DarkTheme';
 import { CssBaseline } from '@mui/material';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline/>
      <Navbar/>
+     <Home/>
     </ThemeProvider>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=''>
+      <section className='relative flex-col justify-center banner -z-50 fles item-center'>
+        <div className='w-[50w] z-10 text-center'>
+          <p className='z-10 py-5 text-2xl lg:text-6xl fond-bold' >Grocery Items</p>
+          <p className='z-10 text-xl text-bg-gray-50 lg:text-4xl'>Buy & Enjoy Your Items</p>
+        </div>
+      </section>
+    
+    
+    
+    </div>
   )
 }
 
