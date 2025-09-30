d7d242a44ee94f0857036291a0c1d582543afa41
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-30 23:43:15 +0530

    Feat:Added top grocery items data

---
+++ src/components/Home/MultiItemCarousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const MultiItemCarousel = () => {
+  return (
+    <div>MultiItemCarousel</div>
+  )
+}
+
+export default MultiItemCarousel
---
+++ src/components/Home/topGrocery.js
@@ -0,0 +1,26 @@
+const topGrocery=[
+{
+    image: "https://images.pexels.com/photos/248412/pexels-photo-248412.jpeg",
+    title: "Milk"
+  },
+  {
+    image: "https://images.pexels.com/photos/209206/pexels-photo-209206.jpeg",
+    title: "Bread"
+  },
+  {
+    image: "https://images.pexels.com/photos/162712/egg-white-food-protein-162712.jpeg",
+    title: "Eggs"
+  },
+  {
+    image: "https://images.pexels.com/photos/326005/pexels-photo-326005.jpeg",
+    title: "Apples"
+  },
+  {
+    image: "https://images.pexels.com/photos/4110256/pexels-photo-4110256.jpeg",
+    title: "Rice"
+  }
+    
+];
+
+export default topGrocery;
+    
