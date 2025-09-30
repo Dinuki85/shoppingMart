3d9e59853a243e91f638a9b3be5f003032dbce88
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-30 23:30:27 +0530

    Fix:Fix the content alignments

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css"
 const Home = () => {
   return (
     <div className=''>
-      <section className='relative flex-col justify-center banner -z-50 fles item-center'>
+      <section className='relative flex flex-col justify-center banner -z-50 item-center'>
         <div className='w-[50w] z-10 text-center'>
           <p className='z-10 py-5 text-2xl lg:text-6xl fond-bold' >Grocery Items</p>
           <p className='z-10 text-xl text-bg-gray-50 lg:text-4xl'>Buy & Enjoy Your Items</p>
