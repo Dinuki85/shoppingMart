988960dfa372fbe37704acd5bfd7355d87437e31
Dinuki85 <prarthanadinuki2@gmail.com>
2025-09-30 23:29:03 +0530

    Feat:Added banner image styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "./Home.css"
 const Home = () => {
   return (
     <div className=''>
@@ -8,6 +8,10 @@ const Home = () => {
           <p className='z-10 py-5 text-2xl lg:text-6xl fond-bold' >Grocery Items</p>
           <p className='z-10 text-xl text-bg-gray-50 lg:text-4xl'>Buy & Enjoy Your Items</p>
         </div>
+        <div className='absolute top-0 left-0 right-0 cover'>
+          
+        </div>
+        <div className='fadout'></div>
       </section>
     
     
