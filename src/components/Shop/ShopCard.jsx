1603e3ceeaec5c057474268fbcc37e4a7cb27b7c
Dinuki85 <prarthanadinuki2@gmail.com>
2025-11-01 13:33:57 +0530

    Feat:Added images

--- src/components/Shop/ShopCard.jsx
+++ src/components/Shop/ShopCard.jsx
@@ -1,8 +1,17 @@
+import { Card } from '@mui/material'
 import React from 'react'
 
 const ShopCard = () => {
   return (
-    <div>ShopCard</div>
+    <Card className='m-5 w-[18rem] '>
+       <div classame={`${true? 'cursor-pointer':"cursor-not-allowed"}'relative`}>
+       <img src="https://pixabay.com/photos/doughnuts-desserts-pastries-treats-1868573/"  alt=""/>
+
+
+        </div>
+
+
+    </Card>
   )
 }
 
