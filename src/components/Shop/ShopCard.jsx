51d3965e9468c87e1dc4809d4527368a9702ca24
Dinuki85 <prarthanadinuki2@gmail.com>
2025-11-01 15:00:13 +0530

    Feat:Added Favourites of the shops

--- src/components/Shop/ShopCard.jsx
+++ src/components/Shop/ShopCard.jsx
@@ -1,5 +1,7 @@
-import { Card } from '@mui/material'
+import { Card, Chip, IconButton } from '@mui/material'
 import React from 'react'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const ShopCard = () => {
   return (
@@ -7,7 +9,26 @@ const ShopCard = () => {
        <div classame={`${true? 'cursor-pointer':"cursor-not-allowed"}'relative`}>
        <img className='w-full h-[10rem] rounded-t-md object-cover'src="https://pixabay.com/photos/doughnuts-desserts-pastries-treats-1868573/"  alt=""/>
 
+        <Chip
+        size = "small"
+        className='absolute top-2 left-2'
+        color={true?"success":"error"}
+        label={true?"open":"closed"}
+        / >
 
+        
+
+        </div>
+        <div className='justify-between w-full p-4 textPart lg:flex'>
+            <p className='text-lg font-semibold'>Sri Lankan Grocery Shops</p>
+            <p className='text-sm text-gray-500'>Dive into your favourite......</p>
+            
+        </div>
+
+        <div>
+            <IconButton>
+                {true?<FavoriteIcon/>:<FavoriteBorderIcon/>}
+            </IconButton>
         </div>
 
 
