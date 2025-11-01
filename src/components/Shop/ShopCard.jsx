58e548248d180518bb8363d53ee1bb1b536cccf5
Dinuki85 <prarthanadinuki2@gmail.com>
2025-11-01 13:35:11 +0530

    Feat:Added styles for the image

--- src/components/Shop/ShopCard.jsx
+++ src/components/Shop/ShopCard.jsx
@@ -5,7 +5,7 @@ const ShopCard = () => {
   return (
     <Card className='m-5 w-[18rem] '>
        <div classame={`${true? 'cursor-pointer':"cursor-not-allowed"}'relative`}>
-       <img src="https://pixabay.com/photos/doughnuts-desserts-pastries-treats-1868573/"  alt=""/>
+       <img className='w-full h-[10rem] rounded-t-md object-cover'src="https://pixabay.com/photos/doughnuts-desserts-pastries-treats-1868573/"  alt=""/>
 
 
         </div>
