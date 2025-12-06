fdea594270bcaf4bcfa5baf6ecc27d9e186bc2cd
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-06 18:01:51 +0530

    Feat:Added Grid image for the Shopping Details page

--- src/components/Shop/ShopDetail.jsx
+++ src/components/Shop/ShopDetail.jsx
@@ -1,9 +1,27 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 const ShopDetail = () => {
-  return (
-    <div>ShopDetail</div>
-  )
+    return (
+        <div className='px-5 lg:px-20'>
+            <section>
+                <h3>Home/Sri Lanka/Grocery Items/3</h3>
+                <div>
+                    <Grid containre spacing={2}>
+                        <Grid item xs={12}>
+                            <img className='w-full h-[40vh]
+                             object-cover' src="https://www.pexels.com/photo/assorted-vegetable-lot-2733918/"
+                              alt="" 
+                              />
+                        </Grid>
+
+                    </Grid>
+                </div>
+            </section>
+        </div>
+
+
+    )
 }
 
 export default ShopDetail
