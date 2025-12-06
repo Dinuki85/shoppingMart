adec186e2dea4492b8b3cfb7463eb496a0e02858
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-06 20:15:31 +0530

    Fix:Fixed the Grid

--- src/components/Shop/ShopDetail.jsx
+++ src/components/Shop/ShopDetail.jsx
@@ -8,47 +8,16 @@ const ShopDetail = () => {
                 <h3 className='mb-4 font-serif font-semibold'>Home/Sri Lanka/Grocery Items/3</h3>
                 <div>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} >
-                            <img 
+                        <Grid item xs={12}  >
+                            <img className="object-cover w-full h-100"
                                 src="https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg"
                                 alt=""
-                                style={{
-                                    width: '100%',
-                                    height:'40vh',
-                                    objectFit:'cover',
-                                    display:'block'
-                                 
-                                }}
+                                
                                
                             />
                         </Grid>
                         
-                        <Grid item xs={12} lg={6}>
-                            <img className="w-full h-[40vh] object-cover rounded-md"
-                                src="https://images.pexels.com/photos/5473023/pexels-photo-5473023.jpeg"
-                                alt=""
-                               style={{
-                                    width: '100%',
-                                    height:'40vh',
-                                    objectFit:'cover',
-                                    display:'block'
-                                 
-                                }}
-                            />
-                        </Grid>
-                        <Grid item xs={12} lg={6}>
-                            <img className="w-full h-[40vh] object-cover rounded-md"
-                                src="https://images.pexels.com/photos/2733918/pexels-photo-2733918.jpeg"
-                                alt=""
-                               style={{
-                                    width: '100%',
-                                    height:'40vh',
-                                    objectFit:'cover',
-                                    display:'block'
-                                 
-                                }}
-                            />
-                        </Grid>
+                        
                        
 
                     </Grid>
