f068e8bf8ff0edb5a6d0e2b7de73c4484c740332
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-06 19:48:57 +0530

    Feat:Added the images of the shop details page

--- src/components/Shop/ShopDetail.jsx
+++ src/components/Shop/ShopDetail.jsx
@@ -5,16 +5,51 @@ const ShopDetail = () => {
     return (
         <div className='px-5 lg:px-20'>
             <section>
-                <h3>Home/Sri Lanka/Grocery Items/3</h3>
+                <h3 className='mb-4 font-serif font-semibold'>Home/Sri Lanka/Grocery Items/3</h3>
                 <div>
-                    <Grid containre spacing={2}>
+                    <Grid container spacing={2}>
                         <Grid item xs={12} >
-                            <img className='w-full h-[40vh]
-                             object-cover' src="https://www.pexels.com/photo/
-                             assorted-vegetable-lot-2733918/"
+                            <img 
+                                src="https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg"
                                 alt=""
+                                style={{
+                                    width: '100%',
+                                    height:'40vh',
+                                    objectFit:'cover',
+                                    display:'block'
+                                 
+                                }}
+                               
                             />
                         </Grid>
+                        
+                        <Grid item xs={12} lg={6}>
+                            <img className="w-full h-[40vh] object-cover rounded-md"
+                                src="https://images.pexels.com/photos/5473023/pexels-photo-5473023.jpeg"
+                                alt=""
+                               style={{
+                                    width: '100%',
+                                    height:'40vh',
+                                    objectFit:'cover',
+                                    display:'block'
+                                 
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12} lg={6}>
+                            <img className="w-full h-[40vh] object-cover rounded-md"
+                                src="https://images.pexels.com/photos/2733918/pexels-photo-2733918.jpeg"
+                                alt=""
+                               style={{
+                                    width: '100%',
+                                    height:'40vh',
+                                    objectFit:'cover',
+                                    display:'block'
+                                 
+                                }}
+                            />
+                        </Grid>
+                       
 
                     </Grid>
                 </div>
