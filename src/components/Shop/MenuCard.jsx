b4d70310b7239e7c853877b45813ba779dfc0ceb
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-07 12:18:23 +0530

    Feat:Added accordion icon from mui

--- src/components/Shop/MenuCard.jsx
+++ src/components/Shop/MenuCard.jsx
@@ -1,10 +1,21 @@
+import { Accordion, AccordionSummary } from '@mui/material'
 import React from 'react'
 
 const MenuCard = () => {
   return (
-    <div>
-        Menu
-    </div>
+     <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography component="span">Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
   )
 }
 
