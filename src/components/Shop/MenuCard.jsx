4b8f661cf0d9ba59ac2bdf2f71b682aeb76c8789
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-07 12:37:07 +0530

    Feat:Added menu details

--- src/components/Shop/MenuCard.jsx
+++ src/components/Shop/MenuCard.jsx
@@ -11,7 +11,21 @@ const MenuCard = () => {
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          <Typography component="span">Accordion 1</Typography>
+          <Typography component="span"> 
+            <div className='items-center justify-between lg:flex'>
+                <div className='items-center lg:flex lg:gap-5' >
+                    <img 
+                    className ='w-[7rem] h-[7rem] object-cover'
+                    src="https://images.pexels.com/photos/1311771/pexels-photo-1311771.jpeg"
+                    alt="" />
+                    <div className='space-y-1 lg:space-y-5 lg:max-w-2xl'>
+                        <p className='text-2xl font-semibold'>Rice</p>
+                        <p>Rs.250.00</p>
+                        <p className="text-gray-500">Red Rice will keep your health in a good way</p>
+                    </div>
+                </div>
+            </div>
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
