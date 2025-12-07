8dce670537a75d252c42549f3bed011d8f2a975a
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-07 15:36:32 +0530

    Feat:added bill details section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,14 +2,45 @@ import { Divider } from '@mui/material'
 import React from 'react'
 import { CartItem } from './CartItem'
 
+const items=[1,1,]
 const Cart = () => {
   return (
     <div>
         <main className='justify-between lg:flex'>
             <section className='lg:w-[30%] space-y-6 lg:min-h-screen pt-10'>
-                <CartItem/>
+                
+              {items.map((item)=> <CartItem/>) }
+              <Divider/>
+              <div className="px-5 text-sm billDetails">
+                <p className = "py-5 font-extralight">Bill Details</p>
+                <div className='space-y-3'>
+                    <div className='flex justify-between text-gray-400'>
+                        <p>Item Total</p>
+                        <p>Rs.999.00</p>
+                        
+                    </div>
+                    <div className='flex justify-between text-gray-400'>
+                        <p>Delive Fee</p>
+                        <p>Rs.30.00</p>
+                        
+                    </div>
+                    <div className='flex justify-between text-gray-400'>
+                        <p>Shop Charges</p>
+                        <p>Rs.60.00</p>
+                        
+                    </div>
+                    <Divider/>
+                </div>
+                <div className='flex justify-between text-gray-400'>
+                    <p>Totak Pay</p>
+                    <p>Rs.50.00</p>
+                </div>
+                       
+              </div>
             </section>
-                <Divider/>
+                
+                
+
 
         </main>
     </div>
