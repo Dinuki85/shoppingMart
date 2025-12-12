e44cd5756ab8d97b7f4db4a456224aab1025b7ac
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-12 07:49:20 +0530

    Feat:Adde favourite card

--- src/components/Profile/Favorites.jsx
+++ src/components/Profile/Favorites.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import ShopCard from '../Shop/ShopCard'
 
 export const Favorites = () => {
   return (
-    <div>Favorites</div>
+    <div className=''>
+        <h1 className='py-5 text-xl font-semibold text-center'>My Favorites</h1>
+    <div className='flex flex-wrap justify-center gap-3'>
+        {
+            [1,1,1,].map((item)=><ShopCard/>)
+        }
+        </div>
+        
+    </div>
   )
 }
