f5069cc473b92dbe80174861e9ace794735ca793
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-12 23:25:08 +0530

    Feat:Added navigation to the redirect to the home page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,8 +23,8 @@ export const Navbar = () => {
         <Box className='flex items-center px-5 sticky top-0 z-50 py-[.8rem] bg-blue-300 lg:px-20 justify-between'>
 
             <div className='flex items-center space-x-5 cursor-pointer lg:mr-10'>
-                <li className='font-serif text-2xl font-extrabold logo text-green-950'>
-                    Grocery Shop
+                <li onClick={()=>navigate("/")} className='font-serif text-2xl font-extrabold logo text-green-950'>
+                    DP
                 </li>
 
 
