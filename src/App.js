5d861d601f0277b20fd5c4bd233f6aebb97273d8
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-12 19:40:11 +0530

    Fix:Fix the import error

--- src/App.js
+++ src/App.js
@@ -8,16 +8,22 @@ import ShopDetail from "./components/Shop/ShopDetail";
 import Cart from "./components/Cart/Cart";
 import Profile from "./components/Profile/Profile";
 import CustomerRouter from "./components/Routers/CustomerRouter";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getUser } from "./components/State/Authentication/Action";
 
 function App() {
+  const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
+  const { auth } = useSelector((store) => store);
+
+  useEffect(() => {
+    dispatch(getUser(auth.jwt || jwt));
+  }, [auth.jwt]);
+
   return (
     <ThemeProvider theme={darkTheme}>
-      <CssBaseline/>
-      <Navbar/>
-      <Home/>
-      {/*<ShopDetail/>*/}
-      {/*<Cart/>*/}
-      {/*<Profile/>*/}
+      <CssBaseline />
       <CustomerRouter />
     </ThemeProvider>
   );
--- src/components/Routers/CustomerRouter.jsx
+++ src/components/Routers/CustomerRouter.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
-import { Home, RouteSharp } from '@mui/icons-material'
+
 import Cart from '../Cart/Cart'
 import ShopDetail from '../Shop/ShopDetail'
 import { Route, Routes } from 'react-router-dom'
 import Profile from '../Profile/Profile'
 import { Auth } from '../Auth/Auth'
+import Home from '../Home/Home'
 
 const CustomerRouter = () => {
   return (
     <div>
-        <Navbar/>
+         <Navbar/>
         <Routes>
+         
           <Route path='/' element={<Home/>}/>
           <Route path='/account/:register' element={<Home/>}/>
           <Route path='/shop/:city/:title/:id' element={<ShopDetail/>}/>
