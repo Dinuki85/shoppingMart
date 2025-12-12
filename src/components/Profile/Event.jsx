b62d838691b5841cff3eb016a31f3ef05e0c1335
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-12 08:06:03 +0530

    Feat:Added event page

--- src/components/Profile/Event.jsx
+++ src/components/Profile/Event.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { EventCard } from './EventCard'
 
 export const Event = () => {
   return (
-    <div>Event</div>
+    <div className='flex flex-wrap gap-5 px-5 mt-5'>
+        {
+            [1,1,1,].map((item)=><EventCard/>)
+        }
+
+    </div>
   )
 }
