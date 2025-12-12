43f6c4a6d316c1e83a0dd70e39f8fd339e430bbd
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-12 08:10:13 +0530

    Fix:Fix the delete icon That is only available for the admin

--- src/components/Profile/EventCard.jsx
+++ src/components/Profile/EventCard.jsx
@@ -1,24 +1,30 @@
-import { Card, CardContent, CardMedia, Typography } from '@mui/material'
+import { Card, CardActions, CardContent, CardMedia, IconButton, Typography } from '@mui/material'
 import React from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export const EventCard = () => {
-  return (
-    <div>
-        <Card sx={{width:345}}>
-            <CardMedia
-            
-            sx={{height:345}} image='https://content.pexels.com/images/canva/ai-generated-ad/off-theme/ocean_pier_with_thatched_restaurant_at_dusk-full.jpg'/>
-    <CardContent>
-        <Typography variant='h5' > Sri Lanka Best Grocery Items</Typography>
-            <Typography variant='body2' > 50% discount</Typography>
-            <div className='py-2 space-y-2'>
-    <p>{"mumbai"}</p>
-    <p className='text-sm text-blue-500'>15.12.2025</p>
-    <p className='text-sm text-red-500'>15.12.2025</p>
-            </div>
+    return (
+        <div>
+            <Card sx={{ width: 345 }}>
+                <CardMedia
 
-    </CardContent>
+                    sx={{ height: 345 }} image='https://content.pexels.com/images/canva/ai-generated-ad/off-theme/ocean_pier_with_thatched_restaurant_at_dusk-full.jpg' />
+                <CardContent>
+                    <Typography variant='h5' > Sri Lanka Best Grocery Items</Typography>
+                    <Typography variant='body2' > 50% discount</Typography>
+                    <div className='py-2 space-y-2'>
+                        <p>{"mumbai"}</p>
+                        <p className='text-sm text-blue-500'>15.12.2025</p>
+                        <p className='text-sm text-red-500'>15.12.2025</p>
+                    </div>
+
+                </CardContent>
+                {false && <CardActions>
+                    <IconButton>
+                        <DeleteIcon />
+                    </IconButton>
+                </CardActions>}
             </Card>
-    </div>
-  )
+        </div>
+    )
 }
