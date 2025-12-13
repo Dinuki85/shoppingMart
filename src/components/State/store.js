30fe9d11e5622aef0a6ac6d0424781e6ca59a326
Dinuki85 <prarthanadinuki2@gmail.com>
2025-12-13 12:21:42 +0530

    Feat:Added Cart Reducer

--- src/components/State/store.js
+++ src/components/State/store.js
@@ -3,11 +3,14 @@ import { authReducer } from "./Authentication/Reducer";
 import { thunk } from "redux-thunk";
 import shopReducer from "./Shop/Reducer";
 import menuItemReducer from "./Menu/Reducer";
+import cartReducer from "./Cart/Reducer";
 
 const rootReducer=combineReducers({
     auth:authReducer,
     shop:shopReducer,
     menu:menuItemReducer,
+    cart:cartReducer,
+    
 
 
 })
